631341dea95b6598373684ed1afd64f22097b182
Guillaume Goujard <guillaume.goujard@polytechnique.edu>
2020-03-25 14:52:46 -0700

    skeleton

---
+++ main/MarketOperator/marketOperator.py
@@ -0,0 +1,5 @@
+
+
+class marketOperator:
+    def __init__(self):
+        pass
---
+++ main/Network/PriceBids/Generator/Generator.py
@@ -0,0 +1,8 @@
+
+class Generator:
+
+    def __init__(self, node_name, type, Pmax=None, Pmin=None):
+        self.node_name = node_name
+        self.type = type
+        self.Pmax = Pmax
+        self.Pmin = Pmin
